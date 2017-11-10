8b6bf2d7881a99eead530758d9b5eab4f6987bdb
David Shlemayev <secondlifedvi@gmail.com>
2017-11-10 22:32:05 +0200

    Fixed 1080p screens

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from cx_Freeze import setup, Executable
 opts = {"packages": ["os", "codecs"], "excludes": ["tkinter"], "include_files": ["editbtn.png", "editbtn_large.png"]}
 
 setup(
-    name = "Roons",
+    name = "LoL Rune Manager",
     version = "1.0",
-    description = "Roons",
+    description = "LoL Rune Manager",
     options = {"build_exe": opts},
     executables = [Executable("import.py", base = "Win32GUI")])
